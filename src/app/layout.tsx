6d5d425a4341b63e204d9118e0d16e855b6b3d93
Arthur Horyu <1horyuarthur@gmail.com>
2025-11-11 14:26:54 +0000

    Quero instalar o page speed insights do vercel no projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Ferdian-MSP',
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
