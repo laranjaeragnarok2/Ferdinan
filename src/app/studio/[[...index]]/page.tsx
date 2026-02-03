f31c0d09b0b9c21245470d1843819c25999b0542
laranjaeragnarok2 <1horyuarthur@gmail.com>
2026-02-03 17:39:16 -0300

    feat: adiciona página para o Sanity Studio e instala a dependência styled-components.

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { NextStudio } from 'next-sanity/studio';
-import config from '../../../../../sanity.config'; // Ajustar caminho relativo para raiz
+import config from '../../../../sanity.config'; // Ajustar caminho relativo para raiz
 
 export const dynamic = 'force-static';
 
